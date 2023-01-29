ec097674835f1657426791fdd170d508b3f89885
Gavin Klibowitz <103222783+CY83R-3X71NC710N@users.noreply.github.com>
2023-01-29 16:33:00 -0600

    Update ultimate-regex-solver.sh

--- ultimate-regex-solver.sh
+++ ultimate-regex-solver.sh
@@ -1,4 +1,4 @@
-# Solve MIT 
+# Solves the 2013 MIT Mystery Hunt, by Dan Gulotta, Palmer Mebane Regex Crossword https://regexcrossword.com/playerpuzzles/8cbea27f-c4c5-4d11-a509-6a622ba01107
 git clone https://github.com/dneiter/regex-crossword
 cd regex-crossword
 python2 main.py
