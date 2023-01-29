e3b7d6e56d58aac8db22d9f8e8cb1b53d09e7f63
Gavin Klibowitz <103222783+CY83R-3X71NC710N@users.noreply.github.com>
2023-01-29 17:52:53 -0600

    Update custom-solver.py

--- custom-solver.py
+++ custom-solver.py
@@ -1,3 +1,4 @@
+# Solves https://regexcrossword.com/playerpuzzles/585e12a824c43
 Echo "Solving Challenge"
 from crossword import solve_crossword
 solve = solve_crossword(["HE|LL|O+","[PLEASE]+"], ["[^SPEAK]+","EP|IP|EF"])
