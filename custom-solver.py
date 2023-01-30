8524297acd6a831b35428d91205266b5fabb73af
Gavin Klibowitz <103222783+CY83R-3X71NC710N@users.noreply.github.com>
2023-01-29 18:20:06 -0600

    Update custom-solver.py

--- custom-solver.py
+++ custom-solver.py
@@ -1,6 +1,14 @@
 # Solves https://regexcrossword.com/playerpuzzles/585e12a824c43
 from crossword import solve_crossword
-echo "Solving Challenge"
-solve = solve_crossword(["HE|LL|O+","[PLEASE]+"], ["[^SPEAK]+","EP|IP|EF"])
-for line in solve:
-    print (''.join(line))
+
+rows = [
+    r'[^XZVCHFJLQM]+',
+]
+
+cols = [
+    r'.*\sA?(SA|PE|N\s){2}',
+]
+
+answer = solve_crossword(rows, cols)
+for line in answer:
+    print ('  '.join(line))
