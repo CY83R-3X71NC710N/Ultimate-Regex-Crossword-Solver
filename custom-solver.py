e4d434c96b92143ac2e6ae80c586f4236ca5c0db
Gavin Klibowitz <103222783+CY83R-3X71NC710N@users.noreply.github.com>
2023-01-29 19:28:45 -0600

    Update custom-solver.py

--- custom-solver.py
+++ custom-solver.py
@@ -1,27 +1,32 @@
 # pip install ply z3-solver
-# https://regexcrossword.com/playerpuzzles/638abba5-5cfa-430c-b26a-33d1f4f42dab
+# https://regexcrossword.com/playerpuzzles/595e8c8e86584
 from crossword import solve_crossword
 
 rows = [
-    r'(.)(.)(.)\3\1\2\1',
-    r'(.)\1(.)\2\1(.)(.)',
-    r'(.)(.)\2\1(.)\3\2',
-    r'(.)(.)\1(.)2\1\3',
-    r'(.)(.)(.)\2\1\2\3',
-    r'(.)(.)\1\2\1(.)\2',
-    r'(.)\1(.)(.)\3\1(.)',
+    r'[^XZVCHFJLQM]+',
+    r'[^PZVJG]{4}(.)[EFUG]{6}\1[^\sPZVJI]{2}',
+    r'[^\sPQFB]{7}[^MGVAJNZ\s]+[^MVZJ]',
+    r'N[OYSRU]{5}[NICE]{6}\s\-',
+    r'.A[A\sDL]{4}O[AECLV\s]+',
 ]
 
 cols = [
-    r'(.)(.)(.)\1(.)\2\4',
-    r'(.)(.)\2(.)\1\3\1',
-    r'(.){2}(.)\1\2(.)3',
-    r'(.)(.)(.)(.)\1\4\1',
-    r'(.)(.)(.)\1(.)4\3',
-    r'(.)\1(.)\1\2(.)\1',
-    r'(.)(.)1(.)\2\2\3',
+    r'.*\sA?(SA|PE|N\s){2}',
+    r'([XYZ])(P|GO|EL)\1.',
+    r'(LS|CA|OS)[L\sP][DO]{2}',
+    r'(U)(T)\2\1[AOB?]',
+    r'(.)\1\1\1\s',
+    r'(FF|BE|QU|OS){2}L',
+    r'ES?F?(OBZ|UCO|PTE)',
+    r'S[MVU]B(TZ|BP|IV)',
+    r'T[GLMV]{2}(E)\1',
+    r'(JK|AE|EN|MG){2}L',
+    r'N(XN|ZB|CA|FS){2}',
+    r'[XHDJ]R[MZVIJ]EC',
+    r'X?W(\sE|OS|PE){2}',
+    r'[VIMZJ]{3}\-\s',
 ]
 
 answer = solve_crossword(rows, cols)
 for line in answer:
-    print ("  ".join(line))
+    print ('  '.join(line))
