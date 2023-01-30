5f6c7e24c55a4ee0716a99e11e043f1a213513ac
Gavin Klibowitz <103222783+CY83R-3X71NC710N@users.noreply.github.com>
2023-01-29 18:32:35 -0600

    Update custom-solver.py

--- custom-solver.py
+++ custom-solver.py
@@ -38,6 +38,11 @@ cols = [
     r'(?!.*\w)(?=^.*\$\^.*$)^\$.*.:.*.\^$',
     r'(?!.*#\$)[$$$ I AM RICH! $$$]*\W*`',
 
+def solve_crossword(rows, cols):
+    # Code to solve crossword puzzle
+
+    return answer
+
 answer = solve_crossword(rows, cols)
 for line in answer:
-    print ('  '.join(line))
+    print(' '.join(line))
